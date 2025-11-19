0790d767fc783b9110ce2bdd843a9e4297988209
Liam <ltconflans@gmail.com>
2025-11-19 12:14:11 +0100

    ouais

--- kaka.js
+++ kaka.js
@@ -1,6 +1,26 @@
-function removeAttaque(){
-    var pikachu = document.getElementById("attaque1");
-    var parent = document.body;
-    parent.removeChild(pikachu)
+function Commentaire(){
+    var a = document.getElementById("oui").value
+    console.log(a)
+    document.getElementById("non").value = a
 }
 
+function Commentaire2(){
+    var pseudo = document.getElementById("pseudo").value
+    var commentaire = document.getElementById("com").value
+        
+    var tableCommentaire = document.getElementById("TableCom")
+    
+    var ligne = document.createElement("tr")
+    var newName = document.createElement("td")
+    var newCom = document.createElement("td")
+
+    newName.innerText = pseudo
+    newCom.innerText = commentaire
+    
+    ligne.appendChild(newName)
+    ligne.appendChild(newCom)
+    
+    tableCommentaire.appendChild(ligne)
+    
+    
+}
