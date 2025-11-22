8acf6852f797c88d8cb83f614eab979602e587e7
Liam Taccon <wengshinliam@gmail.com>
2025-11-23 00:36:58 +0100

    date

--- kaka.js
+++ kaka.js
@@ -2,18 +2,28 @@ function Commentaire2(){
     var pseudo = document.getElementById("pseudo").value
     var commentaire = document.getElementById("com").value
         
+    var date = new Date()
+
+    j = date.getDate().toString()
+    m = date.getMonth().toString()
+    a = date.getFullYear().toString()
+
+    
     var tableCommentaire = document.getElementById("TableCom")
     
     var ligne = document.createElement("tr")
     var newName = document.createElement("td")
     var newCom = document.createElement("td")
+    var newDate = document.createElement("td")
+    
 
     newName.innerText = pseudo
     newCom.innerText = commentaire
+    newDate.innerText = j + "/" + m + "/" + a
     
     ligne.appendChild(newName)
     ligne.appendChild(newCom)
-    
+    ligne.appendChild(newDate)
     tableCommentaire.appendChild(ligne)
     
     document.getElementById("pseudo").value = "quelqu'un"
